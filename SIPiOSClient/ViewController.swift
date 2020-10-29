f356cea1f35a6519bec6fd3d2240a248af4caaf0
baek <zealot109@naver.com>
2020-10-29 16:26:34 +0900

    first project init

---
+++ SIPiOSClient/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SIPiOSClient
+//
+//  Created by SungWoo Back on 2020/10/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
